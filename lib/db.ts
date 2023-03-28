256f5dc22ebe75e3ae27c7647f31c8c7c361e738
Bharat Soni <bharatchills@gmail.com>
2023-03-28 08:28:08 +0530

    db helper function

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+//caching our Prisma client and reuse it when possible to avoid having too many connections
+
+
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
